44d527963d30c29d9068381f822949cb3eecc811
Samoboz <bozcelik40@gmail.com>
2025-04-19 00:07:33 +0200

    aufgabe 4 hinzugefügt

---
+++ Übung4/Aufgabe4/data.js
@@ -0,0 +1,21 @@
+const articles = [
+  {
+    title: "CSS Grundlagen",
+    date: "17. April 2025 11:00 Uhr",
+    author: "Lisa Meier",
+    intro: "Ein Einstieg in CSS für Webseiten.",
+    content:
+      "CSS (Cascading Style Sheets) ist für das Design von Webseiten zuständig...",
+    tags: ["CSS", "Design", "Frontend"],
+  },
+  {
+    title: "JavaScript Basics",
+    date: "16. April 2025 09:30 Uhr",
+    author: "Jonas Bauer",
+    intro: "Was ist JavaScript und wozu nutzt man es?",
+    content: "JavaScript ist eine Skriptsprache für dynamische Webseiten...",
+    tags: ["JavaScript", "Programmierung", "Web"],
+  },
+];
+
+window.articles = articles;
